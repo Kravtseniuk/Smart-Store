d6fe3f5d76db36af63afed2080aa23f90c15518b
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2023-01-25 01:07:00 +0200

    [add] ShoppingCart

---
+++ SmartStore/Models/ShoppingCart.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartStore.Models
+{
+    public class ShoppingCart
+    {
+        public int ProductId { get; set; }
+    }
+}
---
+++ SmartStore/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SmartStore.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IList<Product> ProductList { get; set; }
+    }
+}
