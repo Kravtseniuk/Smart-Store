87880cce844118c4c696259410b458f83cb8f20f
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2023-01-24 22:12:13 +0200

    [add] DetailsVM and HomeVM for Home Page

---
+++ SmartStore/Models/ViewModels/DetailsVM.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartStore.Models.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            Product = new Product();
+        }
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
+    }
+}
---
+++ SmartStore/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SmartStore.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
