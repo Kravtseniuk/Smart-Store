82fc2545bfcf931504070297c398bcaaf9258087
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2023-01-25 23:04:23 +0200

    ApplicationUser Repository added

---
+++ SmartStore_DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using SmartStore_DataAccess.Repository.IRepository;
+using SmartStore_Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartStore_DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+    }
+}
---
+++ SmartStore_DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using SmartStore_Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartStore_DataAccess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+    }
+}
