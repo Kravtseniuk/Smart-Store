9d9d12c8902e2b4ae55f5dbb4d998580ee26b1a1
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2023-01-28 01:54:45 +0200

    [add] Controller and ViewModel for order list management

---
+++ SmartStore/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using SmartStore_DataAccess.Repository.IRepository;
+using SmartStore_Utility.BrainTree;
+
+namespace SmartStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+
+
+        public OrderController(IOrderHeaderRepository orderHRepo,
+            IOrderDetailRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _orderHRepo = orderHRepo;
+            _orderDRepo = orderDRepo;
+            _brain = brain;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartStore_Models/ViewModels/OrderListVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartStore_Models.ViewModels
+{
+    public class OrderListVM
+    {
+        public IEnumerable<OrderHeader> OrderHList { get; set; }
+        public IEnumerable<SelectListItem> StatusList { get; set; }
+        public string Status { get; set; }
+    }
+}
