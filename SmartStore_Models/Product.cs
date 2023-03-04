c785f472cba72f03fc600562d699a3fb76129d60
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2023-03-04 16:38:54 +0200

    Update Product.cs

--- SmartStore_Models/Product.cs
+++ SmartStore_Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartStore_Models
@@ -14,15 +15,20 @@ namespace SmartStore_Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Назва")]
         public string Name { get; set; }
 
+        [DisplayName("Короткий опис")]
         public string ShortDesc { get; set; }
-        
+
+        [DisplayName("Характеристики")]
         public string Description { get; set; }
-        
+
+        [DisplayName("Ціна")]
         [Range(1, int.MaxValue)]
         public double Price { get; set; }
-        
+
+        [DisplayName("Фото")]
         public string Image { get; set; }
         
         [Display(Name = "Категорія")]
@@ -32,7 +38,7 @@ namespace SmartStore_Models
         public virtual Category Category { get; set; }
 
         [NotMapped]
-        [Range(1,10000)]
+        [Range(1,10000, ErrorMessage = "Кількість товару має бути більше 0")]
         public int TempQuantity { get; set; }
     }
 }
