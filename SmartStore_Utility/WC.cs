771ce3f56ba242b28bf68db3d103793939accdf6
Ihor <kravtseniuk@gmail.com>
2023-06-27 21:28:28 +0300

    [add] web constant for images

---
+++ SmartStore_Utility/WC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStore_Utility
+{
+    public class WC
+    {
+        public const string ImagePath = @"\images\product\";
+        public const string CategoryName = "Category";
+    }
+}
