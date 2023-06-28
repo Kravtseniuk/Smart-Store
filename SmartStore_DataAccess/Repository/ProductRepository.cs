23a86f3271546b20007cc44b974846df440a0bc0
Ihor <kravtseniuk@gmail.com>
2023-06-28 20:25:56 +0300

    [add] Product model and Repository for them

---
+++ SmartStore_DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SmartStore_DataAccess.Data;
+using SmartStore_DataAccess.Repository.IRepository;
+using SmartStore_Models;
+using SmartStore_Utility;
+
+namespace SmartStore_DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetAllDropdownList(string obj)
+        {
+            if (obj == WC.CategoryName)
+            {
+                return _db.Categories.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+            }
+
+            return null;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+
+        public IEnumerable<Product> SearchProductsByName(string searchString)
+        {
+            return _db.Products.Where(p => p.Name.Contains(searchString)).ToList();
+        }
+    }
+}
--- SmartStore_Models/Product.cs
+++ SmartStore_Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -14,21 +15,30 @@ namespace SmartStore_Models
         [Key]
         public int Id { get; set; }
 
+        [DisplayName("Назва")]
         public string Name { get; set; }
 
+        [DisplayName("Бренд")]
         public string Brand { get; set; }
 
+        [DisplayName("Опис")]
         public string Description { get; set; }
 
+        [DisplayName("Ціна")]
         public double Price { get; set; }
 
+        [ValidateNever]
+        [DisplayName("Фото")]
         public string Image { get; set; }
 
+        [DisplayName("Категорія товару")]
         public int CategoryId { get; set; }
 
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public virtual Category Category { get; set; }
 
+        [ValidateNever]
         public virtual ICollection<ProductAttribute> ProductAttributes { get; set; }
 
     }
