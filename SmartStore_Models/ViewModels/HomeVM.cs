67a468f1873f9f6e5206e60fc019155eeb17e9e1
Ihor <kravtseniuk@gmail.com>
2023-07-17 03:00:26 +0300

    [add] Home Pages

---
+++ SmartStore_Models/ViewModels/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStore_Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<ProductAttribute> ProductAttributes { get; set; }
+    }
+}
