ac8d1c3f559f1b84cf25891acd3a8f2c62640a4d
Ihor <kravtseniuk@gmail.com>
2023-07-23 19:51:18 +0300

    Update ApplicationUser.cs

--- SmartStore_Models/ApplicationUser.cs
+++ SmartStore_Models/ApplicationUser.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,19 @@ namespace SmartStore_Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FullName { get; set; }
+
+        [NotMapped]
+        public string StreetAddress { get; set; }
+        
+        [NotMapped]
+        public string City { get; set; }
+
+        [NotMapped]
+        public string State { get; set; }
+
+        [NotMapped]
+        public string PostalCode { get; set; }
     }
 }
